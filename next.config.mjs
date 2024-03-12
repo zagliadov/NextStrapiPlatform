b8b7415ea324bd06148f6015e5cf56eb66f46136
Daniil <portal.sid.grand2@gmail.com>
2024-03-12 09:51:15 +0200

    fix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
-import path from "path";
 /** @type {import('next').NextConfig} */
-const currentDir = path.resolve('.');
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false, path: false };
-    }
-
-    config.node = {
-      __dirname: true,
-    };
-    config.resolve.alias['@'] = path.resolve(currentDir);
-
-    return config;
-  },
   env: {
     STRAPI_URL: process.env.STRAPI_URL,
   },
